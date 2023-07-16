41f1cb6123bdaec773b084362064b0d5f73adff7
Jim Barrett <jimbarrett27@gmail.com>
2023-07-16 17:21:44 +0200

    add a dummy paper route

--- app_backend/routes.py
+++ app_backend/routes.py
@@ -1,5 +1,34 @@
 from app_backend import app
+import numpy as np
 
-@app.route('/hello_world')
-def hello_world():
-    return {'hello': 'world'}
+@app.route('/random_dummy_paper')
+def random_dummy_paper():
+
+    papers = [
+        {
+            "title": "A title",
+            "authorList": "A et al",
+            "publicationDate": "17th July 1991",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "
+        },
+        {
+            "title": "Another title",
+            "authorList": "B et al",
+            "publicationDate": "8th December 2020",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "
+        },
+        {
+            "title": "Yet another title",
+            "authorList": "c et al",
+            "publicationDate": "3rd February 2016",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "
+        },
+        {
+            "title": "Such title, much paper",
+            "authorList": "D et al",
+            "publicationDate": "15th October 1955",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  "
+        }
+    ]
+
+    return np.random.choice(papers, size=2)
