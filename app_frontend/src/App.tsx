7be5a24f60d637ca734cfd7e8da8f342e45eeb7d
Jim Barrett <jimbarrett27@gmail.com>
2023-10-29 17:26:33 +0100

    lint and fix the frontend; try and include js linting in the CICD pipeline

--- app_frontend/src/App.tsx
+++ app_frontend/src/App.tsx
@@ -1,79 +1,79 @@
-import React from 'react';
-import './App.css';
-import { useState } from 'react';
+import './App.css'
+import React, { useState } from 'react'
 
-
-type Paper = {
-  title: string,
-  publicationDate: string,
-  authorList: string,
+interface Paper {
+  title: string
+  publicationDate: string
+  authorList: string
   abstract: string
 
 }
 
-function SearchField({setPapersInView}: {setPapersInView : (papers: Paper[]) => void}) {
-  
-  const fetchPapersForSearchTerm = async () => {
-    const searchBox = document.getElementById('searchBox');
+function SearchField ({ setPapersInView }: { setPapersInView: (papers: Paper[]) => void }): React.ReactElement {
+  // const fetchPapersForSearchTerm = async (): Promise<void> => {
+  const fetchPapersForSearchTerm = (): void => {
+    const searchBox = document.getElementById('searchBox')
 
-    let searchTerm = '';
-    if (searchBox) {
-      searchTerm = searchBox.innerText;
+    let searchTerm = ''
+    if (searchBox !== null) {
+      searchTerm = searchBox.innerText
     }
 
     const postBody = {
-      'search_term': searchTerm,
-    };
-
-    console.log(postBody);
+      search_term: searchTerm
+    }
 
     fetch(
-        '/get_closest_papers_to_search_term',
-        {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify(postBody),
-        },
+      '/get_closest_papers_to_search_term',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(postBody)
+      }
     )
-        .then( (resp) => {
-          return resp.json();
-        })
-        .then((papers) => {
-          setPapersInView(papers);
-        });
-  };
+      .then(async (resp) => {
+        return await resp.json()
+      })
+
+      .then((papers) => {
+        setPapersInView(papers)
+      }, (reason) => {
+        console.error(reason)
+        setPapersInView([])
+      })
+  }
 
   return (
     <>
       <input id="searchBox" type="text" />
       <button onClick={fetchPapersForSearchTerm} >Search</button>
     </>
-  );
+  )
 }
 
-function PaperSummaries( {papers}: {papers: Paper[]} ) {
-  if (papers.length === 0) return <></>;
+function PaperSummaries ({ papers }: { papers: Paper[] }): React.ReactElement {
+  if (papers.length === 0) return <></>
 
   return <>{papers.map((paper: Paper) => {
     return (<>
       <PaperSummary paper={paper} />
       <br></br>
-    </>);
-  })}</>;
+    </>)
+  })}</>
 }
 
-function PaperSummary({paper}: {paper: Paper}) {
+function PaperSummary ({ paper }: { paper: Paper }): React.ReactElement {
   return (
     <>
       <h2>{paper.title}</h2>
       <h3><i>{paper.publicationDate}</i> - {paper.authorList}</h3>
       <p>{paper.abstract}</p>
     </>
-  );
+  )
 }
 
-export default function MyApp() {
-  const [papersInView, setPapersInView] = useState<Paper[]>([]);
+export default function MyApp (): React.ReactElement {
+  const [papersInView, setPapersInView] = useState<Paper[]>([])
 
   return (
     <div>
@@ -81,6 +81,5 @@ export default function MyApp() {
       <SearchField setPapersInView={setPapersInView} />
       <PaperSummaries papers={papersInView} />
     </div>
-  );
+  )
 }
-
